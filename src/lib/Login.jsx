baf98331d6b0053744219f6f12eacded9a1d8eed
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2023-06-01 11:29:01 +0530

    added form

--- src/lib/Login.jsx
+++ src/lib/Login.jsx
@@ -1,24 +1,70 @@
 import React from 'react'
 import logo from './logo.png'
+import '../App.css'
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
-    <div>
-        <div className='logo'>
-          <img src={logo}></img>
-        </div>
-        <div className='form'>
-          <div>
+    <div className='main_box'>
+      <div className='logo'>
+        <img src={logo}></img>
+      </div>
+      <div className='form'>
+
+        <div>
           <h1>Login</h1>
           <p>welcome to .Please login to your account</p>
-          </div>
-          <form>
-            <label>College id</label>
-            <input ></input>
-            <label>Password</label>
-            <input ></input>
-          </form>
         </div>
+        <form>
+        
+          <div>
+            <label>College id</label>
+            <TextField id="outlined-basic" label="id@pvppcoe.ac.in" variant="outlined" />
+          </div>
+
+          <div>
+            <label>Password</label>
+            <FormControl  variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </div>
+        </form>
+      </div>
 
     </div>
   )
